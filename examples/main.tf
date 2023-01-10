15de78cab1277c03086befa1e9d34eabb0636a22
Rahid Owu <122138511+rahidowu@users.noreply.github.com>
2023-01-10 21:05:03 +0600

    Add files via upload

---
+++ examples/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region  = "${var.aws_region}"
+  profile = "${var.aws_profile}"
+  version = "~> 1.9"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "${var.aws_profile}"
+  alias   = "us-east-1"
+  version = "~> 1.9"
+}
+
+module "single-page-application" {
+  source  = "li0nel/single-page-application/aws"
+  version = "0.0.7"
+
+  stack_name  = "${var.stack_name}"
+  aws_profile = "${var.aws_profile}"
+  aws_region  = "${var.aws_region}"
+  domain_name = "${var.domain_name}"
+}
